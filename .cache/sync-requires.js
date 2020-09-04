17b4c4ad39d4f87a601efb53bb77fca3cd5609a5
Jan Lishak <35602700+janlishak@users.noreply.github.com>
2020-09-04 20:53:06 +0200

    check content-seassion-1 for more info

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---node-modules-wkocjan-gatsby-theme-intro-src-pages-404-js": hot(preferDefault(require("/home/jan/projects/janlishak.github.io/node_modules/@wkocjan/gatsby-theme-intro/src/pages/404.js"))),
-  "component---node-modules-wkocjan-gatsby-theme-intro-src-templates-index-js": hot(preferDefault(require("/home/jan/projects/janlishak.github.io/node_modules/@wkocjan/gatsby-theme-intro/src/templates/index.js")))
+  "component---node-modules-wkocjan-gatsby-theme-intro-src-pages-404-js": hot(preferDefault(require("/myhome/jan/janlishak.github.io/node_modules/@wkocjan/gatsby-theme-intro/src/pages/404.js"))),
+  "component---node-modules-wkocjan-gatsby-theme-intro-src-templates-index-js": hot(preferDefault(require("/myhome/jan/janlishak.github.io/node_modules/@wkocjan/gatsby-theme-intro/src/templates/index.js")))
 }
 
